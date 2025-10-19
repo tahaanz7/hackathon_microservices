242833cc2b1ce980b0c8fb5395b97416f51ba670
Taha Anz <tahaanz70@gmail.com>
2025-10-19 17:23:38 +0100

    polish code

--- hack-web-api/routes/hacksRoutes.js
+++ hack-web-api/routes/hacksRoutes.js
@@ -3,53 +3,14 @@ const express = require('express');
 const router = express.Router();
 const hacksController = require('../controllers/hacksController');
 
-
-
-let hacks = [];
-
-//get all
-// router.get('/', (req,res)=>{
-//     if(!hacks){
-//         res.send("hacks is empty!");
-//     }
-//     res.status(200).json(hacks);
-// })
 router.get('/', hacksController.getAllHacks);
 
 //create hack
 router.post('/', hacksController.createHack)
 
 
-//get by id
-// router.get('/:id', (req, res)=>{
-//     const id = parseInt(req.params.id);
-
-//     const hack = hacks.find(h => h.id == id);
-
-//     if(!hack){
-//         return res.status(404).json({message:'hack not found!'});
-//     }
-
-//     res.status(200).json(hack);
-// })
 router.get('/:id', hacksController.getHackById);
 
-//delete
-// router.delete('/:id',(req, res)=>{
-//      const id = parseInt(req.params.id);
-
-//     const index = hacks.findIndex(h => h.id == id);
-
-//     if (index === -1) {
-//          return res.status(404).json({ message: 'Hack not found!' });
-//     }
-    
-
-//     hacks.splice(index, 1);
-
-//     res.status(200).json({message:"deleted successfully!"})
-
-// })
 router.delete('/:id',hacksController.deleteHack);
 
 //update
